fb43ac0715195c081fffe47fff96aa2aca5f508d
akihiro-hosoya <iicina.yftj.33@gmail.com>
2020-04-06 23:28:18 +0900

    レッスン

--- forum/urls.py
+++ forum/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from forum import views
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='top'),
+    path('', views.IndexView.as_view(), name='home'),
     path('posts/', views.PostListView.as_view(), name='post_list'),
 ]
