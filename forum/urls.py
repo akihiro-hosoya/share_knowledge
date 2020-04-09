e94ccc8056570abc56c931cfd1fb9491cc55497c
akihiro-hosoya <iicina.yftj.33@gmail.com>
2020-04-09 19:43:02 +0900

    公開機能まで

--- forum/urls.py
+++ forum/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('posts/', views.PostListView.as_view(), name='post_list'),
     path('post/new/', views.CreatePostView.as_view(), name='post_new'),
     path('post/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
+    path('post/drafts/', views.DraftListView.as_view(), name='post_draft_list'),
+    path('post/<int:pk>/publish/', views.post_publish, name='post_publish'),
+    path('post/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post_edit'),
 ]
--- forum/views.py
+++ forum/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
 from forum.models import Post
-from django.views.generic import (TemplateView, ListView, CreateView, DetailView)
+from django.views.generic import (TemplateView, ListView, CreateView, DetailView, UpdateView)
 from forum.forms import PostForm
 from django.utils import timezone
 
@@ -26,3 +26,21 @@ class CreatePostView(CreateView):
 class PostDetailView(DetailView):
     model = Post
     template_name = "forum/post_detail.html"
+
+class DraftListView(ListView):
+    model = Post
+    template_name = "forum/post_draft_list.html"
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__isnull=True).order_by('created_date')
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+
+class PostUpdateView(UpdateView):
+    template_name = "forum/post_form.html"
+    redirect_field_name = 'forum/post_detail.html'
+    form_class = PostForm
+    model = Post
