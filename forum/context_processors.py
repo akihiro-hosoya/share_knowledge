3ded5a8e14340c93e26e8ad45ff8052f685956e0
akihiro-hosoya <iicina.yftj.33@gmail.com>
2020-04-29 17:56:39 +0900

    新着情報欄作成

--- forum/context_processors.py
+++ forum/context_processors.py
@@ -1,9 +1,11 @@
-from .models import Category, ParentCategory, GrandCategory
+from .models import Category, ParentCategory, GrandCategory, Post, Comment, NewsPost
 
 def common(request):
     context = {
         'grand_list': GrandCategory.objects.all(),
         'parent_list': ParentCategory.objects.all(),
         'category_list': Category.objects.all(),
+        'news_list': NewsPost.objects.order_by('-published_date')[:3],
     }
     return context
+
